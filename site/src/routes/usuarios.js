a734a01869c1f2f5f53832e7fb0504b1b65a38fe
AlyssonAlmeidaSantos <100138927+AlyssonAlmeidaSantos@users.noreply.github.com>
2022-06-07 12:02:49 -0300

    utlimas alterações

--- site/src/routes/usuarios.js
+++ site/src/routes/usuarios.js
@@ -3,20 +3,24 @@ var router = express.Router();
 
 var usuarioController = require("../controllers/usuarioController");
 
-router.get("/", function (req, res) {
+router.get("/", function(req, res) {
     usuarioController.testar(req, res);
 });
 
-router.get("/listar", function (req, res) {
+router.get("/listar", function(req, res) {
     usuarioController.listar(req, res);
 });
 
 //Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
-router.post("/cadastrar", function (req, res) {
+router.post("/cadastrar", function(req, res) {
     usuarioController.cadastrar(req, res);
 })
 
-router.post("/autenticar", function (req, res) {
+router.post("/registrar", function(req, res) {
+    usuarioController.registrar(req, res);
+})
+
+router.post("/autenticar", function(req, res) {
     usuarioController.entrar(req, res);
 });
 
